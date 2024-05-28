be5baf58b20b62ef4e922d1d1b233bccb3e0f9b5
FlechaMaker <flechacreador@gmail.com>
2024-05-28 16:50:15 +0900

    Use whisper (OpenAI API)

--- install.sh
+++ install.sh
@@ -6,16 +6,17 @@ EXTENSION_NAME=Speech2Scratch
 EXTENSION_ID=speech2scratch
 COLLABORATOR=champierre
 EXTENSION_DESCRIPTION="Speech Recognition Blocks."
+EXTENSION_DIR=node_modules/${EXTENSION_ID}
 
 mkdir -p node_modules/scratch-vm/src/extensions/scratch3_${EXTENSION_ID}
-cp ${EXTENSION_ID}/scratch-vm/src/extensions/scratch3_${EXTENSION_ID}/index.js node_modules/scratch-vm/src/extensions/scratch3_${EXTENSION_ID}/
+cp ${EXTENSION_DIR}/scratch-vm/src/extensions/scratch3_${EXTENSION_ID}/index.js node_modules/scratch-vm/src/extensions/scratch3_${EXTENSION_ID}/
 mv node_modules/scratch-vm/src/extension-support/extension-manager.js node_modules/scratch-vm/src/extension-support/extension-manager.js_orig
 sed -e "s|class ExtensionManager {$|builtinExtensions['${EXTENSION_ID}'] = () => require('../extensions/scratch3_${EXTENSION_ID}');${LF}${LF}class ExtensionManager {|g" node_modules/scratch-vm/src/extension-support/extension-manager.js_orig > node_modules/scratch-vm/src/extension-support/extension-manager.js
 
-mkdir -p src/lib/libraries/extensions/${EXTENSION_ID}
-cp ${EXTENSION_ID}/scratch-gui/src/lib/libraries/extensions/${EXTENSION_ID}/${EXTENSION_ID}.png src/lib/libraries/extensions/${EXTENSION_ID}/
-cp ${EXTENSION_ID}/scratch-gui/src/lib/libraries/extensions/${EXTENSION_ID}/${EXTENSION_ID}-small.png src/lib/libraries/extensions/${EXTENSION_ID}/
-mv src/lib/libraries/extensions/index.jsx src/lib/libraries/extensions/index.jsx_orig
+mkdir -p node_modules/scratch-gui/src/lib/libraries/extensions/${EXTENSION_ID}
+cp ${EXTENSION_DIR}/scratch-gui/src/lib/libraries/extensions/${EXTENSION_ID}/${EXTENSION_ID}.png node_modules/scratch-gui/src/lib/libraries/extensions/${EXTENSION_ID}/
+cp ${EXTENSION_DIR}/scratch-gui/src/lib/libraries/extensions/${EXTENSION_ID}/${EXTENSION_ID}-small.png node_modules/scratch-gui/src/lib/libraries/extensions/${EXTENSION_ID}/
+mv node_modules/scratch-gui/src/lib/libraries/extensions/index.jsx node_modules/scratch-gui/src/lib/libraries/extensions/index.jsx_orig
 DESCRIPTION="\
     {${LF}\
         name: '${EXTENSION_NAME}',${LF}\
@@ -35,4 +36,4 @@ DESCRIPTION="\
         internetConnectionRequired: true,${LF}\
         bluetoothRequired: false${LF}\
     },"
-sed -e "s|^export default \[$|import ${EXTENSION_ID}IconURL from './${EXTENSION_ID}/${EXTENSION_ID}.png';${LF}import ${EXTENSION_ID}InsetIconURL from './${EXTENSION_ID}/${EXTENSION_ID}-small.png';${LF}${LF}export default [${LF}${DESCRIPTION}|g" src/lib/libraries/extensions/index.jsx_orig > src/lib/libraries/extensions/index.jsx
+sed -e "s|^export default \[$|import ${EXTENSION_ID}IconURL from './${EXTENSION_ID}/${EXTENSION_ID}.png';${LF}import ${EXTENSION_ID}InsetIconURL from './${EXTENSION_ID}/${EXTENSION_ID}-small.png';${LF}${LF}export default [${LF}${DESCRIPTION}|g" node_modules/scratch-gui/src/lib/libraries/extensions/index.jsx_orig > node_modules/scratch-gui/src/lib/libraries/extensions/index.jsx
